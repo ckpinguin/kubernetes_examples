b19b7e6f9762fff7cabd598d50089aa680d0f67b
Christof Kälin <christof.kaelin@gmail.com>
2020-09-14 11:49:04 +0200

    formatting

--- traefik/bootstrap_traefik.sh
+++ traefik/bootstrap_traefik.sh
@@ -3,7 +3,7 @@ echo "Creating Traefik objects:"
 microk8s kubectl apply -f traefik_crd.yml
 microk8s kubectl apply -f traefik_rbac_clusterrole_and_binding.yml
 microk8s kubectl apply -f traefik_serviceaccount_and_deployment.yml
-#microk8s kubectl apply -f traefik_serviceaccount_daemonset_and_service.yml
+#microk8s kubectl apply -f traefik_v1.7_serviceaccount_daemonset_and_service.yml
 microk8s kubectl apply -f traefik_web_ui_service_and_ingress.yml
 
 echo "Waiting 60s for pods to stabilize..."
@@ -12,3 +12,5 @@ echo "Forwarding Kubernetes-Dashboard to localhost (this is needed before any In
 microk8s kubectl port-forward --address 0.0.0.0 service/traefik 8000:8000 8080:8080 8443:4443 -n kube-system&
 #microk8s kubectl port-forward -n kube-system service/traefik-web-ui-svc 8888:8080
 echo "You can view the Traefik Web UI at https://localhost:8080/dashboard"
+
+exit 0
